e362147e682805e9f43b396838cf6f76f16ccb6c
Bohdan <bogmira777@gmail.com>
2023-11-26 10:19:36 +0100

    Add animated text task

--- Lesson1/animated-text/js/script.js
+++ Lesson1/animated-text/js/script.js
@@ -1,10 +1,21 @@
 "use strict";
 
 const addTextButton = document.querySelector(".btn-add-text");
+const clearTextButton = document.querySelector(".btn-clear-text");
+
+let delayTime = 150
 
 addTextButton.addEventListener("click", () => {
-  addText("block", "12345678910");
+  const wordsArray = ["Treasure", "Adventure", "Map", "Discovery"];
+  const randomWord = getRandomWord(wordsArray);
+  const blockId = addTextButton.getAttribute("data-text-id");
+  addText(blockId, randomWord);
 });
+
+function getRandomWord(wordsArray) {
+  const randomIndex = Math.floor(Math.random() * wordsArray.length);
+  return wordsArray[randomIndex];
+}
 
 function addText(id, text) {
   const element = document.getElementById(id);
@@ -15,7 +26,12 @@ function addText(id, text) {
     let timer;
 
     function addLetter() {
-      element.innerHTML += text[counter];
+      const randomColor = getRandomColor();
+      const letterSpan = document.createElement("span");
+      letterSpan.innerHTML = text[counter];
+      letterSpan.style.color = randomColor;
+      element.appendChild(letterSpan);
+
       counter++;
 
       if (counter >= text.length) {
@@ -23,6 +39,25 @@ function addText(id, text) {
       }
     }
 
-    timer = setInterval(addLetter, 200);
+    timer = setInterval(addLetter, delayTime);
+  }
+}
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+clearTextButton.addEventListener("click", clearText);
+
+function clearText() {
+  let blockId = clearTextButton.getAttribute("data-text-id");
+  const element = document.getElementById(blockId);
+  if (element) {
+    element.innerHTML = "";
   }
 }
